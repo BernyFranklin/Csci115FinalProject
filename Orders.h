2b78d08be6c1bc973155bf3fdf2191aba6a9f07b
Frank Bernal <frankbernal89@gmail.com>
2024-12-04 12:46:42 -0800

    Added Orders.h
    This is the header for creating an order object

---
+++ Orders.h
@@ -0,0 +1,30 @@
+#ifndef CSCI115FINALPROJECT_ORDERS_H
+#define CSCI115FINALPROJECT_ORDERS_H
+#include <string>
+using namespace std;
+
+class Order {
+private:
+    string id;
+    int priority;
+    char destination;
+
+public:
+    Order(const string& orderId, int orderPriority, const string& orderDestination);
+    Order();
+
+    // setters
+    string getId() const;
+    int getPriority() const;
+    string getDestination() const;
+
+    // getters
+    void setId(const string& orderId);
+    void setPriority(int orderPriority);
+    void setDestination(const string& orderDestination);
+
+    // displayers
+    void displayOrder() const;
+};
+
+#endif //CSCI115FINALPROJECT_ORDERS_H
