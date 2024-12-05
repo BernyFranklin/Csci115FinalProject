c56fbf930a9ada373220456bc058264df7adbfff
Frank Bernal <frankbernal89@gmail.com>
2024-12-05 14:17:57 -0800

    Linked list implementation written with add and display functions

---
+++ SinglyOrderList.cpp
@@ -0,0 +1,44 @@
+#include "SinglyOrderList.h"
+#include <iostream>
+
+using namespace std;
+
+// constructor
+SinglyOrderList::SinglyOrderList() : head(nullptr), size(0) {}
+
+// destructor
+SinglyOrderList::~SinglyOrderList() {
+    Node* current = head;
+    while (current) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;                // delete each node
+    }
+}
+
+// add order to list
+void SinglyOrderList::addOrder(const Order &order) {
+    Node* newNode = new Node(order);            // create new node
+    if (!head) {
+        // if empty, new node is head
+        head = newNode;
+    }
+    else {
+        // traverse to the end
+        Node* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next - newNode;
+    }
+    size++;                                     // increment the size
+}
+
+// display all orders in the list
+void SinglyOrderList::displayOrders() const {
+    Node* current = head;
+    while (current) {
+        current->data.displayOrder();           // Use Order's displayOrder function
+        current = current->next;
+    }
+}
