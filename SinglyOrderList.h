a236231bf2ac9d03fa7884f63e86cdf1e69470fd
Frank Bernal <frankbernal89@gmail.com>
2024-12-05 14:09:38 -0800

    Class declaration added

---
+++ SinglyOrderList.h
@@ -0,0 +1,27 @@
+#ifndef SINGLY_ORDER_LIST_H
+#define SINGLY_ORDER_LIST_H
+#include "Orders.h"
+
+class SinglyOrderList {
+private:
+    struct Node {
+        Order data;         // order object as node data for simplicity
+        Node* next;         // pointer to the next node
+
+        // node constructor
+        Node(const Order& order, Node* nextNode = nullptr)
+            : data(order), next(nextNode) {}
+    };
+
+    Node* head;             // pointer to the head of the list
+    int size;               // number of nodes on the list
+
+public:
+    SinglyOrderList();      // constructor
+    ~SinglyOrderList();     // destructor
+
+    void addOrder(const Order& order);      // add order to the list
+    void displayOrders() const;             // display all orders in the list
+};
+
+#endif
