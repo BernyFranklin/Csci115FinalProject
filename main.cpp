57e74effe077ff8d22d9bd00e728be4d0ad42ca8
Frank Bernal <frankbernal89@gmail.com>
2024-12-05 13:10:42 -0800

    Tested ArrayOrderList, Works 4.0
    We tested the loading of the array, as well as adding an order. Works 4.0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include "Orders.h"
+#include "ArrayOrderList.h"
 
 using namespace std;
 
 int main() {
+    string orderFile = "/Users/frankbernal/CLionProjects/Csci115FinalProject/sampleOrders.txt";
+    // create the array
+    ArrayOrderList orderList;
+    // load current values from list
+    orderList.loadFromFile(orderFile);
+    // display the loaded array
+    cout << "Orders loaded from file:" << endl;
+    orderList.displayOrders();
+    // test of adding a new order
+    orderList.addOrder(Order("ORD51", 3, "Z"));
+    cout << "After adding a new order:" << endl;
+    orderList.displayOrders();
 
     return 0;
 }
